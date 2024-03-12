2a8400ecb6d7ff8ed9dd9a1c4e970b3c288f904a
Rodrigo Molina <rodrigo.molina@escriba.com.br>
2024-03-12 20:46:26 -0300

    Adicionado bootstrap

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar bg-dark">
+  <div class="container p-3">
+    <span>FOXQUIZZ</span>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+.container {
+  color: rgb(170, 170, 170)
+}
+</style>
